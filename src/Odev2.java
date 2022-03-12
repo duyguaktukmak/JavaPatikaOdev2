5a21a4ce85686fe4fa224e4a79605fec8b72e27d
duygu <duygu.aktokmak@gmail.com>
2022-03-12 18:20:16 +0300

    first commit

--- src/Odev2.java
+++ src/Odev2.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Odev2 {
+import java.util.Scanner;
+
+public class Odev2 {
+    public static void main(String[] args) {
+     double tutar,kdvOranı;
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("Tutarı giriniz:");
+        tutar=input.nextDouble();
+        if (tutar>=0 && tutar<=1000){
+            kdvOranı=0.18;
+            double kdvTutar= tutar*kdvOranı;
+        double kvdlifiyat= tutar+kdvTutar;
+            System.out.println("Kdv Oranı 0.18 iken yeni fiyat:"+ kvdlifiyat);
+        }
+        else if(tutar>1000){
+        kdvOranı=0.8;
+        double kdvTutar= tutar*kdvOranı;
+        double kvdlifiyat= tutar+kdvTutar;
+        System.out.println("Kdv Oranı 0.8 iken yeni fiyat:"+ kvdlifiyat);
+        }
+        else{
+            System.out.println("Boş Değer");
+        }
+    }
 }
